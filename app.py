33b9770b2688c7934d4b84c7dde45b78c5002ffb
henryxie <henryxie2000@gmail.com>
2023-10-15 02:06:11 +0300

    [update] single request completed

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from flask import Flask, request, jsonify
-# from cal.index import calculate
 import cal.generate_pdf as generate_pdf
-from selenium import webdriver
-# from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 from tabledef import *
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
@@ -17,26 +13,6 @@ CORS(application)
 engine = create_engine('sqlite:///user.db?check_same_thread=False', echo=True)
 Session = sessionmaker(bind=engine)
 
-# options = Options()
-# options.add_argument("--ignore-certificate-errors")
-# options.add_argument('disable-infobars')
-# options.add_experimental_option("excludeSwitches", ["enable-automation"])
-# options.add_experimental_option("prefs", {"download.prompt_for_download": False})
-# # options.add_argument("--headless") # Ensure GUI is off
-# options.add_argument("--no-sandbox")
-# options.add_argument("--disable-dev-shm-usage")
-
-# profile = {"plugins.plugins_list": [{ "enabled": False, "name": "Chrome PDF Viewer"}],
-#     # 'profile.default_content_settings.popups': 0,
-#     # 'profile.content_settings.exceptions.automatic_downloads.*.setting': 1,
-#     "download.default_directory": "",
-#     "download.extensions_to_open": "",
-#     "plugins.always_open_pdf_externally": True
-# }
-
-# options.add_experimental_option("prefs", profile)
-# driver = webdriver.Chrome(options = options)
-
 @application.route('/')
 def hello():
     print("hello world")
@@ -48,10 +24,10 @@ def scraping():
     method = request.json
     rfc = method["rfc"]
     password = method["password"]
+    file_name = method["filename"]
 
     s = Session()
     result = s.query(User).filter_by(rfc = rfc).first()
-    print("--->result", result)
 
     now = datetime.now()
     current_time = now.strftime("%Y-%m-%d %H:%M:%S")
@@ -66,30 +42,23 @@ def scraping():
         difference_in_seconds = (current_dt - prev_dt).total_seconds()
 
         if difference_in_seconds <= 7 * 24 * 60 * 60:
-            print("==pwd==", password)
-            print("==result pwd==", result.password)
             if password == result.password:
                 return jsonify(result.data)
-            else: return jsonify({"status": "Claves incorrectas", "pdfbase64": "", "url": "1", "data": {}})
+            else: 
+                return jsonify({"status": "Claves incorrectas", "pdfbase64": "", "url": "1", "data": {}})
         else:
             if data['status'] != 'OK':
                 return jsonify(data)    
             else:
-                print('=======pdf result=======', data)
                 result.data = data
                 result.created_date = current_dt
                 s.commit()
                 return jsonify(s.query(User).filter_by(rfc = rfc).first().data)
     else:
-        data = generate_pdf_intance.login(rfc, password)
-
-        print('--------------- data result ---------------', data)
+        data = generate_pdf_intance.login(rfc, password, file_name)
         if data['status'] != 'OK':
             return jsonify(data)
         else:
-            print("rfc", rfc)
-            print("password", password)
-            print("current_dt", current_dt)
             user = User(rfc, password, data, current_dt)
             s.add(user)
             s.commit()
--- cal/generate_pdf.py
+++ cal/generate_pdf.py
@@ -6,6 +6,7 @@ from pyzbar import pyzbar
 from PIL import Image
 import base64
 import time
+from bs4 import BeautifulSoup
 from re import fullmatch
 from urllib.parse import quote
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,23 +17,56 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import os
+from dotenv import load_dotenv
 
 class GeneratePdf():
     def __init__(self):
+        self.result = {
+            "status": "claves incorrectas",
+            "pdfbase64": "",
+            "url": "",
+            "data": {}
+        }
+        
         # Install Webdriver
         service = Service(ChromeDriverManager().install())
-        chrome_options = Options()
-        if os.name == 'nt':
-            path = os.getcwd()+'\sat_gob'
-        else:
-            path = os.getcwd()+'/sat_gob'
 
-        chrome_options.add_argument('--user-data-dir=' + path)
+        current_directory = os.getcwd()
+        download_directory = os.environ.get('DOWNLOAD_DIRECTORY')
+        download_full_path = os.path.join(current_directory, download_directory)
+
+        os.makedirs(download_full_path, exist_ok=True)
 
         try:
+            options = Options()
+            if os.name == 'nt':
+                path = os.getcwd()+'\sat_gob'
+            else:
+                path = os.getcwd()+'/sat_gob'
+
+            options.add_argument('--user-data-dir=' + path)
+            options.add_argument("--ignore-certificate-errors")
+            options.add_argument('disable-infobars')
+            options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            options.add_experimental_option("prefs", {"download.prompt_for_download": False})
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+
+            profile = {
+                "plugins.plugins_list": [{ "enabled": False, "name": "Chrome PDF Viewer"}],
+                # 'profile.default_content_settings.popups': 0,
+                # 'profile.content_settings.exceptions.automatic_downloads.*.setting': 1,
+                "download.default_directory": download_full_path,
+                "download.extensions_to_open": "",
+                "plugins.always_open_pdf_externally": True
+            }
+
+            options.add_experimental_option("prefs", profile)
+
             # Create Driver Instance
-            driver = webdriver.Chrome(options=chrome_options, service=service)
-            driver.get('https://www.sat.gob.mx/aplicacion/login/53027/genera-tu-constancia-de-situacion-fiscal')
+            driver = webdriver.Chrome(options=options, service=service)
+            driver.get(os.environ.get('TARGET_URL'))
 
             self.driver = driver
             self.wait_10 = WebDriverWait(driver, 10)
@@ -43,230 +77,167 @@ class GeneratePdf():
             driver.quit()
             print("Error: Driver loading failed")
 
-    def login(self, rfc, pwd):
-        print('login....')
-        rfc_input = self.wait_10.until(EC.presence_of_element_located(
-                (By.XPATH, '//*[@id="IDPLogin"]/div[1]/div/input')))
-        rfc_input.click()
-        rfc_input.clear()
-        rfc_input.send_keys(rfc)
+    def login(self, rfc, pwd, file_name):
+        try:
+            self.driver.switch_to.frame("iframetoload")
+            rfc_input = self.driver.find_element(By.ID, "rfc")
+            pwd_input = self.driver.find_element(By.ID, "password")
+            captcha_input = self.driver.find_element(By.ID, "userCaptcha")
+            captcha_img_container = self.driver.find_element(By.ID, "divCaptcha")
+            captcha_img = captcha_img_container.find_element(By.TAG_NAME, "img")
+            img_url = captcha_img.get_attribute("src")
+            submit = self.driver.find_element(By.ID, "submit")
 
-        print('rfc_input=', rfc_input)
-        self.wait_15 = WebDriverWait(self.driver, 15)
+            # RFC 
+            rfc_input.click()
+            rfc_input.clear()
+            rfc_input.send_keys(rfc)
 
+            # PASSWORD
+            pwd_input.click()
+            pwd_input.clear()
+            pwd_input.send_keys(pwd)
 
+            # CAPTCHA
+            captcha_url = os.environ.get('CAPTCHA_URL')
+            querystring = {"image": img_url}
 
-# def calculate(userName, userPwd):
-#     pdf_directory = "temp_pdf"
+            rapid_key = ["1b029f7bebmsh8f4d7158c2e53c4p1ec3cdjsn90e7c23929a9", 
+                        "5d10341094mshd1b5b7c721aeb1bp13afbbjsndb2f7b388bea",
+                        "dab759d9a5msh1308f96aedcabf5p113f87jsne21d78b4fd35",
+                        "b8e83ed90bmsh94f9d07bd1d6534p11c4adjsn662fb0fc2217",
+                        "8906cb5d84mshf935efec6c55168p11b99cjsn634fc5c2d963",
+                        "272d06c69amsh5aa1736d50bef91p16068fjsnd0ea6f959383",
+                        "67442fa9c7msh806839ab19f6ad0p1368b1jsnb4d255dd4d07",
+                        "5cb81df867mshf203d18a905e6c7p1b7448jsn500250db89b1",
+                        "04dce39de8mshe7d2a832209f6b8p16b517jsnce7c7000cb28",
+                        "851e4be954msh83309512be5bc12p14661cjsn26bb348c05bf"]
 
-#     options = Options()
-#     options.add_argument("--ignore-certificate-errors")
-#     options.add_argument('disable-infobars')
-#     options.add_experimental_option("excludeSwitches", ["enable-automation"])
-#     options.add_experimental_option("prefs", {"download.prompt_for_download": False})
-#     options.add_argument("--headless") # Ensure GUI is off
-#     options.add_argument("--no-sandbox")
-#     options.add_argument("--disable-dev-shm-usage")
+            for i in rapid_key:
+                try:
+                    headers = {
+                        "X-RapidAPI-Key": i,
+                        "X-RapidAPI-Host": "metropolis-api-captcha.p.rapidapi.com"
+                    }
+                    response = requests.get(captcha_url, headers=headers, params=querystring)
+                    captcha_code = response.json()["captcha"]
+                    break
+                except:
+                    print(f"An error occurred")
+            
+            print("captcha", captcha_code)
 
-#     profile = {"plugins.plugins_list": [{ "enabled": False, "name": "Chrome PDF Viewer"}],
-#         # 'profile.default_content_settings.popups': 0,
-#         # 'profile.content_settings.exceptions.automatic_downloads.*.setting': 1,
-#         "download.default_directory": "temp_pdf",
-#         "download.extensions_to_open": "",
-#         "plugins.always_open_pdf_externally": True
-#     }
+            captcha_input.click()
+            captcha_input.clear()
+            captcha_input.send_keys(captcha_code)
 
-#     options.add_experimental_option("prefs", profile)
-#     driver = webdriver.Chrome(options = options)
+            # SUBMIT
+            submit.click()
+            
+            time.sleep(1)
 
+            # HANDLE ERROR
+            try:
+                error_msg = self.driver.find_element(By.ID, "msgError")
+                if error_msg:
+                    self.driver.quit()
+                    return self.result
+            except:
+                return self.generatePDF(file_name)
 
-#     data = {
-#         "status": "claves incorrectas",
-#         "pdfbase64": "",
-#         "url": "",
-#         "data": {}
-#     }
+        except:
+            self.driver.quit()
+            return self.result
 
-
-#     # Install Webdriver
-#     service = Service(ChromeDriverManager().install())
-#     chrome_options = Options()
-#     if os.name == 'nt':
-#         path = os.getcwd()+'\sat_gob'
-#     else:
-#         path = os.getcwd()+'/sat_gob'
-
-#     chrome_options.add_argument('--user-data-dir=' + path)
-
-#     # Create Driver Instance
-#     driver = webdriver.Chrome(options=chrome_options, service=service)
-
-#     driver.get('https://www.sat.gob.mx/aplicacion/login/53027/genera-tu-constancia-de-situacion-fiscal')
-
-#     try:
-#         print('driver=', driver)
-#         print("Starting")
-#         # # driver.get("https://www.sat.gob.mx/aplicacion/login/53027/genera-tu-constancia-de-situacion-fiscal")
-
-#         # print("Sucess to run")
-#         iframe1 = driver.find_element("id", "iframetoload")
-#         driver.switch_to.frame(iframe1)
-#         nameInput = driver.find_element(By.ID, "rfc")
-#         pwdInput = driver.find_element(By.ID, "password")
-#         captchaInput = driver.find_element(By.ID, "userCaptcha")
-#         submit = driver.find_element(By.ID, "submit")
-#         imageContainer = driver.find_element(By.ID, "divCaptcha")
-#         captchaImage = imageContainer.find_element(By.TAG_NAME, "img")
-#         imgUrl = captchaImage.get_attribute("src")
-#         url = "https://metropolis-api-captcha.p.rapidapi.com/solve"
-
-#         print('iframe1=', iframe1)
-#         print('nameInput=', nameInput)
-#         print('pwdInput=', pwdInput)
-#         print('captchaInput=', captchaInput)
-#         print('submit=', submit)
-#         print('imageContainer=', imageContainer)
-#         print('captchaImage=', captchaImage)
-#         print('imgUrl=', imgUrl)
-
-#         # querystring = {"image": imgUrl}
-
-#         # rapid_key = ["1b029f7bebmsh8f4d7158c2e53c4p1ec3cdjsn90e7c23929a9", 
-#         #             "5d10341094mshd1b5b7c721aeb1bp13afbbjsndb2f7b388bea",
-#         #             "dab759d9a5msh1308f96aedcabf5p113f87jsne21d78b4fd35",
-#         #             "b8e83ed90bmsh94f9d07bd1d6534p11c4adjsn662fb0fc2217",
-#         #             "8906cb5d84mshf935efec6c55168p11b99cjsn634fc5c2d963",
-#         #             "272d06c69amsh5aa1736d50bef91p16068fjsnd0ea6f959383",
-#         #             "67442fa9c7msh806839ab19f6ad0p1368b1jsnb4d255dd4d07",
-#         #             "5cb81df867mshf203d18a905e6c7p1b7448jsn500250db89b1",
-#         #             "04dce39de8mshe7d2a832209f6b8p16b517jsnce7c7000cb28",
-#         #             "851e4be954msh83309512be5bc12p14661cjsn26bb348c05bf"]
-
-#         # for i in rapid_key:
-#         #     try:
-#         #         headers = {
-#         #             "X-RapidAPI-Key": i,
-#         #             "X-RapidAPI-Host": "metropolis-api-captcha.p.rapidapi.com"
-#         #         }
-#         #         response = requests.get(url, headers=headers, params=querystring)
-#         #         captchaCode = response.json()["captcha"]
-#         #         break
-#         #     except:
-#         #         print(f"An error occurred")
+    def generatePDF(self, file_name):
         
-#         # print("captcha", captchaCode)
-
-#         # nameInput.clear()
-#         # pwdInput.clear()
-#         # captchaInput.clear()
-
-#         # nameInput.send_keys(userName)
-#         # pwdInput.send_keys(userPwd)
-#         # captchaInput.send_keys(captchaCode)
-
-#         # submit.click()
-
-#         # time.sleep(0.5)
-#         # try:
-#         #     error_msg = driver.find_element(By.ID, "msgError")
-#         #     if error_msg:
-#         #         data = {
-#         #             "status": "claves incorrectas",
-#         #             "pdfbase64": "",
-#         #             "url": "",
-#         #             "data": {}
-#         #         }
-#         #         return data
-#         # except:
-#         #     pass
-
-#         # try:
-#         #     os.remove(pdf_directory + "/SAT.pdf")
-#         # except Exception as e:
-#         #     print("===Error===", e)
-
-#         # driver.switch_to.default_content()
-
-#         # WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "iframetoload")))
-#         # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "formReimpAcuse:j_idt50"))).click()
-
-#         # driver.switch_to.window(driver.window_handles[-1])
-
-#         # url = driver.current_url
-#         # time.sleep(10)
-
-#         # print("url", url)
-#         # doc = fitz.open(pdf_directory + "/SAT.pdf")
-#         # for page_num in range(len(doc)):
-#         #     page = doc[page_num]
-
-#         #     # Convert the PDF page to an image
-#         #     pix = page.get_pixmap()
-#         #     img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-
-#         #     # Decode QR codes in the image
-#         #     qr_codes = pyzbar.decode(img)
-
-#         #     # Process the detected QR codes
-#         #     flag = 1
-#         #     for qr_code in qr_codes:
-#         #         code = qr_code.data.decode("utf-8")
-#         #         print("QR Code Data:", code)
-#         #         flag = 0
-#         #         break
-#         #     if flag == False: break
-
-#         # with open(pdf_directory + "/SAT.pdf", "rb") as file:
-#         #     encoded_pdf = base64.b64encode(file.read()).decode('utf-8')
+        pdf_path = os.environ.get('DOWNLOAD_DIRECTORY')
+        downloaded_org_file = f"{pdf_path}/SAT.pdf"
+        renamed_file = f"{pdf_path}/{file_name}.pdf"
         
-#         # data["status"] = "OK"
-#         # data["pdfbase64"] = str(encoded_pdf)
-#         # data['url'] = str(code)
+        # HANDLE PDF
+        # try:
+        #     os.remove(downloaded_org_file)
+        # except Exception as e:
+        #     print("===Error===", e)
 
-#         # print("Starting Scraping...")
-#         # driver.get(data['url'])
-#         # html = driver.page_source
-#         # soup = BeautifulSoup(html, 'html.parser')
+        # Switch to main window
+        self.driver.switch_to.default_content()
+        WebDriverWait(self.driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "iframetoload")))
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.ID, "formReimpAcuse:j_idt50"))).click()
 
-#         # td_elements = soup.find_all('td', attrs={'role': 'gridcell'})
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        current_url = self.driver.current_url
+        
+        # Wait download file
+        time.sleep(15)
 
-#         # regi_value = ""
-#         # fecha_de_value = ""
-#         # reg_temp_arr = []
-#         # for td in td_elements:
-#         #     span = td.find('span')
+        # Rename file
+        os.rename(downloaded_org_file, renamed_file)
 
-#         #     if span is not None:
-#         #         if span.text: 
-#         #             temp = str(span.text)
-#         #             temp = temp.replace(" ", "_")
-#         #             temp = temp.replace(":", "")
-#         #             temp = temp.lower()
-#         #     else: 
-#         #         if td.text: 
-#         #             if(temp == 'régimen'):
-#         #                 regi_value = td.text
-#         #             elif(temp == 'fecha_de_alta'):
-#         #                 fecha_de_value = td.text
-#         #             else:
-#         #                 data['data'][temp] = td.text
+        # Handle new pdf file
+        doc = fitz.open(renamed_file)
 
-#         #     if regi_value and fecha_de_value:
-#         #         reg_temp_arr.append({"régimen": regi_value, "fecha_de_alta": fecha_de_value})
-#         #         regi_value = ""
-#         #         fecha_de_value = ""
-#         # data['data']['características_fiscales'] = reg_temp_arr
+        print("pdf page len=", len(doc))
+        for page_num in range(len(doc)):
+            page = doc[page_num]
 
-#         # driver.quit()
+            # Convert the PDF page to an image
+            pix = page.get_pixmap()
+            img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
 
-#         # return data
-#     except Exception as e:
-#         print("error0", e)
-#         data = {
-#             "status": e,
-#             "pdfbase64": "",
-#             "url": "",
-#             "data": {}
-#         }
-#         driver.quit()
-#         return data
+            # Decode QR codes in the image
+            qr_codes = pyzbar.decode(img)
+
+            # Process the detected QR codes
+            flag = 1
+            for qr_code in qr_codes:
+                code = qr_code.data.decode("utf-8")
+                print("QR Code Data:", code)
+                flag = 0
+                break
+            if flag == False: break
+
+        with open(renamed_file, "rb") as file:
+            encoded_pdf = base64.b64encode(file.read()).decode('utf-8')
+        
+        self.result["status"] = "OK"
+        self.result["pdfbase64"] = str(encoded_pdf)
+        self.result['url'] = str(code)
+
+        print("Starting Scraping...")
+        self.driver.get(self.result['url'])
+        html = self.driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        td_elements = soup.find_all('td', attrs={'role': 'gridcell'})
+
+        regi_value = ""
+        fecha_de_value = ""
+        reg_temp_arr = []
+        for td in td_elements:
+            span = td.find('span')
+
+            if span is not None:
+                if span.text: 
+                    temp = str(span.text)
+                    temp = temp.replace(" ", "_")
+                    temp = temp.replace(":", "")
+                    temp = temp.lower()
+            else: 
+                if td.text: 
+                    if(temp == 'régimen'):
+                        regi_value = td.text
+                    elif(temp == 'fecha_de_alta'):
+                        fecha_de_value = td.text
+                    else:
+                        self.result['data'][temp] = td.text
+
+            if regi_value and fecha_de_value:
+                reg_temp_arr.append({"régimen": regi_value, "fecha_de_alta": fecha_de_value})
+                regi_value = ""
+                fecha_de_value = ""
+        self.result['data']['características_fiscales'] = reg_temp_arr
+
+        self.driver.quit()
+        return self.result
